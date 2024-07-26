7f6daf463c1f87c27892c59f4a272666e7b62787
philcurtis422 <philcurtis422@gmail.com>
2024-07-26 07:50:37 -0400

    initial

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+\c postgres
+
+DROP IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+CREATE TABLE departments (
+	id SERIAL PRIMARY KEY,
+	department_name VARCHAR(30)
+);
+
+CREATE TABLE roles (
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(30),
+	salary DECIMAL NOT NULL,
+	department INT NOT NULL,
+	FOREIGN KEY department REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+	id SERIAL PRIMARY KEY,
+	first_name VARCHAR(30),
+	last_name VARCHAR(30),
+	role_id INT NOT NULL,
+	manager_id INT,
+	FOREIGN KEY role_id REFERENCES roles(id),
+	FOREIGN KEY manager_id REFERENCES employees(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO departments (department_name) VALUES
+	('engineering'),
+	('finance'),
+	('legal'),
+	('sales');
+
+INSERT INTO roles (title, salary, department) VALUES
+	('lead sales', 90000, 'sales'),
+	('salesperson', 60000, 'sales'),
+	('lead engineer', 100000, 'engineering'),
+	('software engineer', 75000, 'engineering'),
+	('account manager', 90000, 'finance'),
+	('accountant', 70000, 'finance'),
+	('legal team lead', 150000, 'legal'),
+	('lawyer', 100000, 'legal');
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES
+	()
+	
