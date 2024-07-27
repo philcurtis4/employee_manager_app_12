0f67a50ffd2298dce82a5e1e24abb456abe556a0
philcurtis422 <philcurtis422@gmail.com>
2024-07-27 08:06:04 -0400

    finished app

--- index.js
+++ index.js
@@ -7,33 +7,48 @@ const MenuSelection = require('./lib/menu');
 
 
 async function initializeMenu () {
-	const answerObj = await MenuSelection.menu();
+	const answerObj = await inquirer.prompt([
+		{
+			type: 'list',
+			name: 'menuChoices',
+			message: 'What would you like to do?',
+			choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role', 'Exit']
+		}
+	
+])
 	
 	
 	switch(answerObj.menuChoices){
 		case 'view all departments':
 			await MenuSelection.viewDepartments();
-			MenuSelection.menu();
+			initializeMenu();
 			break;
 		case 'view all roles':
 			await MenuSelection.viewRoles();
-			MenuSelection.menu();
+			initializeMenu();
 			break;
 		case 'view all employees':
 			await MenuSelection.viewEmployees();
-			MenuSelection.menu();
+			initializeMenu();
 			break;
 		case 'add a department':
 			await MenuSelection.showAddDepartmentPrompt();
-			MenuSelection.menu();
+			initializeMenu();
 			break;
 		case 'add a role':
 			await MenuSelection.showAddRolePrompt();
-			MenuSelection.menu();
+			initializeMenu();
 			break;
 		case 'add an employee':
 			await MenuSelection.showAddEmployeePrompt();
-			MenuSelection.menu();
+			initializeMenu();
+			break;
+		case 'update an employee role':
+			await MenuSelection.showUpdateEmployeePrompt();
+			initializeMenu();
+		case 'Exit':
+			console.log('\nThanks for using the Employee Manager App!\n');
+			process.exit();
 	}
 }
 
